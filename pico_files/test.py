8442a76751a090342c9e60d14e0fb8a27f7b1fbd
William <132473775+william-niq@users.noreply.github.com>
2024-02-05 15:44:48 +0100

    removed simple test script, no longer needed

--- pico_files/test.py
+++
@@ -1,8 +0,0 @@
-import json
-SETTING_FILE = "./settings.json"
-with open(SETTING_FILE, "r") as f:
-    settings = json.load(f)
-    f.close()
-
-for key, value in settings.items():
-    print(key, value)
